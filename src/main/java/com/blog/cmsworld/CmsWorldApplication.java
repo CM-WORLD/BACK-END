a8799e017793e62338dbca450d50abdb5d4b75df
jinvicky <jinvicky@intube.kr>
2023-12-19 08:58:57 +0900

    Feat: initial commit

---
+++ src/main/java/com/blog/cmsworld/CmsWorldApplication.java
@@ -0,0 +1,13 @@
+package com.blog.cmsworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmsWorldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blog/cmsworld/CmsWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.cmsworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
