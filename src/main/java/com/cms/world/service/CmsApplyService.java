0dba35d4b74812df8aa501a41d3c7e2023e8a5b5
jinvicky <jinvicky17@gmail.com>
2023-12-21 15:50:32 +0900

    Feat: 로그 기록

--- src/main/java/com/cms/world/service/CmsApplyService.java
+++ src/main/java/com/cms/world/service/CmsApplyService.java
@@ -3,10 +3,12 @@ package com.cms.world.service;
 
 import com.cms.world.domain.dto.CmsApplyDto;
 import com.cms.world.domain.dto.CmsApplyImgDto;
+import com.cms.world.domain.dto.TimeLogDto;
 import com.cms.world.domain.vo.ApplySrchVo;
 import com.cms.world.domain.vo.CmsApplyVo;
 import com.cms.world.repository.CmsApplyImgRepository;
 import com.cms.world.repository.CmsApplyRepository;
+import com.cms.world.repository.TimeLogRepository;
 import com.cms.world.utils.GlobalStatus;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
@@ -24,12 +26,16 @@ public class CmsApplyService {
 
     private final S3UploadService uploadService;
 
-    public CmsApplyService(CmsApplyRepository repository, CmsApplyImgRepository imgRepository, S3UploadService uploadService) {
+    private final TimeLogRepository timeLogRepository;
+
+    public CmsApplyService(CmsApplyRepository repository, CmsApplyImgRepository imgRepository, S3UploadService uploadService, TimeLogRepository timeLogRepository) {
         this.repository = repository;
         this.imgRepository = imgRepository;
         this.uploadService = uploadService;
+        this.timeLogRepository = timeLogRepository;
     }
 
+    /* 커미션 신청 */
     @Transactional
     public int insert(CmsApplyVo vo) {
         try {
@@ -47,12 +53,15 @@ public class CmsApplyService {
                 CmsApplyImgDto imgDto = CmsApplyImgDto.builder().applyDto(dto).imgUrl(awsUrl).build();
                 imgRepository.save(imgDto);
             }
+            recordLog(dto);
+
             return GlobalStatus.EXECUTE_SUCCESS.getStatus();
         } catch (IOException e) {
             return GlobalStatus.EXECUTE_FAILED.getStatus();
         }
     }
 
+    /* 커미션 전체 리스트 조회 (등록 최신순) */
     public List<CmsApplyDto> list (ApplySrchVo vo) {
         List<CmsApplyDto> list = repository.findAll(Sort.by("regDate"));
         //TODO:: regDate 하드코딩이라 바꾸고 싶다.
@@ -65,10 +74,19 @@ public class CmsApplyService {
         try {
             CmsApplyDto dto = repository.findById(id).get();
             dto.setStatus(status);
+            recordLog(dto);
+
             return GlobalStatus.EXECUTE_SUCCESS.getStatus();
         } catch (Exception e) {
             return GlobalStatus.EXECUTE_FAILED.getStatus();
         }
+    }
+
+
+    /* 로그 기록 */
+    public void recordLog (CmsApplyDto dto) {
+        TimeLogDto timeDto= TimeLogDto.builder().status(dto.getStatus()).applyDto(dto).build();
+        timeLogRepository.save(timeDto);
     }
 
 }
