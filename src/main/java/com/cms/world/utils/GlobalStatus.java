d28c0a9f5d3c598d0fc6eee1e64796a9a5e12414
jinvicky <jinvicky17@gmail.com>
2023-12-21 12:22:21 +0900

    Feat: Status 추가

--- src/main/java/com/cms/world/utils/GlobalStatus.java
+++ src/main/java/com/cms/world/utils/GlobalStatus.java
@@ -11,7 +11,12 @@ public enum GlobalStatus {
 
     SUCCESS(200, "성공"),
     INVALID_PARAMETER(400, "잘못된 요청"),
-    INTERNAL_SERVER_ERR(500, "서버 에러");
+    INTERNAL_SERVER_ERR(500, "서버 에러"),
+
+
+    EXECUTE_SUCCESS(1, "실행 성공"),
+    EXECUTE_FAILED(0, "실행 실패");
+
 
     private final int status;
     private final String msg;
