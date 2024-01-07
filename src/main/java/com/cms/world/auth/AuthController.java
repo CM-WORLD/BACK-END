04c1196cb7dceac4bb4db9195f073130b2642b84
jinvicky <jinvicky17@gmail.com>
2024-01-07 20:18:59 +0900

    Feat: 카카오 로그인 개선

--- src/main/java/com/cms/world/auth/AuthController.java
+++ src/main/java/com/cms/world/auth/AuthController.java
@@ -1,12 +1,13 @@
 package com.cms.world.auth;
 
 import com.cms.world.auth.jwt.AuthTokens;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class AuthController {
@@ -16,20 +17,19 @@ public class AuthController {
         this.oAuthLoginService = oAuthLoginService;
     }
 
-    @GetMapping("/social/kakao")
-    public String loginKakaoGet(String code, HttpServletResponse response) {
+    /* 프론트로부터 카카오 인가 코드를 받아서 처리한다. */
+    @PostMapping("/process/kakao")
+    public Map<String, Object> kakaoLogin(@RequestBody Map<String, Object> codeMap) throws JsonProcessingException {
         KakaoLoginParams params = new KakaoLoginParams();
+
+        String code = String.valueOf(codeMap.get("code"));
         params.setAuthorizationCode(code);
 
         AuthTokens jwtToken = oAuthLoginService.login(params);
 
-
-        /* 로그인은 성공했으나 이후 redirect에 관한 프론트와의 처리가 안됨.... TODO */
-        Cookie cookie = new Cookie("cmsUser", jwtToken.getAccessToken());
-        System.out.println("jwtToken = " + jwtToken.getAccessToken());
-        cookie.setMaxAge(3600); // 쿠키의 유효 시간 (초 단위)
-        response.addCookie(cookie);
-
-        return "redirect:";
+        Map<String, Object> map = new HashMap<>();
+        map.put("result", "success");
+        map.put("tokens", jwtToken);
+        return map;
     }
 }
