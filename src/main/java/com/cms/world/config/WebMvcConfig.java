27ce2774df3cee2512c05fcb548002e383d1c32d
jinvicky <jinvicky17@gmail.com>
2024-01-07 20:18:46 +0900

    Feat: interceptor 개선

--- src/main/java/com/cms/world/config/WebMvcConfig.java
+++ src/main/java/com/cms/world/config/WebMvcConfig.java
@@ -1,7 +1,7 @@
 package com.cms.world.config;
 
 
-import com.cms.world.aop.BasicInterceptor;
+import com.cms.world.aop.AuthInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -13,11 +13,17 @@ import java.util.List;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    private final AuthInterceptor authInterceptor;
+    public WebMvcConfig (AuthInterceptor authInterceptor) {
+        this.authInterceptor = authInterceptor;
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 
-        List<String> URL_PATTERNS = Arrays.asList("/apply/form", "/apply/list");
-        registry.addInterceptor(new BasicInterceptor())
+        List<String> URL_PATTERNS = Arrays.asList("/apply/auth/**");
+//        registry.addInterceptor(new AuthInterceptor()) // 이거 대신 위 주입 사용
+        registry.addInterceptor(authInterceptor)
                 .addPathPatterns(URL_PATTERNS)
                 .excludePathPatterns("/css/**", "/images/**", "/js/**");
 
