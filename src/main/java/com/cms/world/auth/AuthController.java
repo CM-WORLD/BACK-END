d18006102ef8344126cc015a28a8b42d99a21aeb
jinvicky <jinvicky17@gmail.com>
2024-01-08 10:43:10 +0900

    Feat: jwt 토큰 발급시 리프레시 토큰 저장, 닉네임 응답값에 추가

--- src/main/java/com/cms/world/auth/AuthController.java
+++ src/main/java/com/cms/world/auth/AuthController.java
@@ -1,7 +1,9 @@
 package com.cms.world.auth;
 
 import com.cms.world.auth.jwt.AuthTokens;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.cms.world.domain.dto.MemberDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,16 +12,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 public class AuthController {
+
     private final OAuthLoginService oAuthLoginService;
 
-    public AuthController(OAuthLoginService oAuthLoginService) {
-        this.oAuthLoginService = oAuthLoginService;
-    }
+    private final RequestOAuthInfoService requestOAuthInfoService;
+
+    private final  MemberRepository memberRepository;
+
 
     /* 프론트로부터 카카오 인가 코드를 받아서 처리한다. */
     @PostMapping("/process/kakao")
-    public Map<String, Object> kakaoLogin(@RequestBody Map<String, Object> codeMap) throws JsonProcessingException {
+    public Map<String, Object> kakaoLogin(@RequestBody Map<String, Object> codeMap) {
         KakaoLoginParams params = new KakaoLoginParams();
 
         String code = String.valueOf(codeMap.get("code"));
@@ -30,6 +35,15 @@ public class AuthController {
         Map<String, Object> map = new HashMap<>();
         map.put("result", "success");
         map.put("tokens", jwtToken);
+
+        //닉네임, 리프레시 토큰 저장
+        OAuthInfoResponse response = requestOAuthInfoService.request(params);
+        MemberDto dto = memberRepository.findByEmail(response.getEmail()).get();
+        dto.setRefreshToken(jwtToken.getRefreshToken()); // 리프레시 토큰 저장
+        memberRepository.save(dto);
+
+        map.put("nick", dto.getNickName());
+
         return map;
     }
 }
